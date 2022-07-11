67084b1fc46311482b9d845f96f896fd5fa51e96
CodexRodney <rodney002mwanje@gmail.com>
2022-07-11 16:55:55 +0300

    MySQL setup test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,11 +7,9 @@ CREATE USER
     IF NOT EXISTS 'hbnb_test'@'localhost'
     IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES
-   ON `hbnb_test_db`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
+   ON hbnb_test_db.*
+   TO 'hbnb_test'@'localhost';
 GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
+   ON performance_schema.*
+   TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
